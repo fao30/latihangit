46804371ac6274a7edc5da4406b2c5d05fb593b4
Alvin Pratama <palveeen22@gmail.com>
2022-11-06 18:17:54 +0300

    already add a new project

--- sandboxalvin.js
+++ sandboxalvin.js
@@ -1,1 +1,17 @@
-console.log("hallo");
+// console.log("hallo");
+// 12345
+// 23451
+// 34512
+// 45123
+// soal satu faoTech 6.11
+let array = [1, 2, 3, 4, 5];
+function nameFunction(array, parameter2) {
+  for (let i = 0; i < parameter2; i++) {
+    // console.log(array[i]);
+    array.push(array.shift());
+    // array.unshift(array.pop());
+    // console.log(array);
+  }
+  return array;
+}
+console.log(nameFunction(array, 4));
