43ba7feb17425d7bef1bc02fdcb3e734609a9901
Ryan Pratama <ru.ryanpratama@gmail.com>
2022-11-06 17:38:11 +0300

    angka diputer puter

--- sandboxryan.js
+++ sandboxryan.js
@@ -1,7 +1,33 @@
-for (let i = 1; i <= 3; i++) {
-  let str = "";
-  for (let j = 1; j <= i; j++) {
-    str += "*";
+// for (let i = 1; i <= 3; i++) {
+//   let str = "";
+//   for (let j = 1; j <= i; j++) {
+//     str += "*";
+//   }
+//   console.log(str);
+// }
+
+// console.log("test");
+// console.log("test kedua");
+
+// 5, 1, 2, 3, 4
+
+let arr = [1, 2, 3, 4, 5];
+let d = 6;
+
+const urutin = (arr, d) => {
+  for (let i = 0; i < d; i++) {
+    let pengurutan = arr[0];
+    arr.shift();
+    arr.push(pengurutan);
   }
-  console.log(str);
-}
+  return arr;
+};
+
+console.log(urutin(arr, d));
+
+// 23451
+// 34512
+// 45123
+// 51234
+// 12345
+// 23451
