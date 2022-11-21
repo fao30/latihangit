15ed8dd314f3ba3193f451e148a0d1484fd9102e
Ryan Pratama <ru.ryanpratama@gmail.com>
2022-11-21 11:14:01 +0300

    21 nov 2022

--- sandboxryan.js
+++ sandboxryan.js
@@ -3914,3 +3914,140 @@ Our solution is written as a function expression and uses string interpolation, 
 
 // team.addGame("Eren", 100, 90);
 // console.log(team.games);
+
+// const checkThatTwoPlusTwoEqualsFourAMillionTimes = () => {
+//   for (let i = 1; i <= 1000000; i++) {
+//     if (2 + 2 != 4) {
+//       console.log("Something has gone very wrong :( ");
+//     }
+//   }
+// };
+
+// // Write your code below
+// const isTwoPlusTwo = checkThatTwoPlusTwoEqualsFourAMillionTimes;
+// isTwoPlusTwo();
+// console.log(isTwoPlusTwo.name);
+
+// const higherOrderFunc = (param) => {
+//   param();
+//   return `I just invoked ${param.name} as a callback function!`;
+// };
+
+// const anotherFunc = () => {
+//   return "I'm being invoked by the higher-order function!";
+// };
+
+// console.log(higherOrderFunc(anotherFunc));
+
+// const addTwo = (num) => {
+//   return num + 2;
+// };
+
+// const checkConsistentOutput = (num, val) => {
+//   let checkA = val + 2;
+//   let checkB = num(val);
+//   if (checkA === checkB) {
+//     return num(val);
+//   } else {
+//     return "inconsistent results";
+//   }
+// };
+
+// console.log(checkConsistentOutput(addTwo, 10));
+
+// const artists = ["Picasso", "Kahlo", "Matisse", "Utamaro"];
+
+// artists.forEach((artist) => {
+//   console.log(artist + " is one of my favorite artists.");
+// });
+
+// const numbers = [1, 2, 3, 4, 5];
+
+// const squareNumbers = numbers.map((number) => {
+//   return number * number;
+// });
+
+// console.log(squareNumbers);
+
+// const things = ["desk", "chair", 5, "backpack", 3.14, 100];
+
+// const onlyNumbers = things.filter((thing) => {
+//   return typeof thing === "number";
+// });
+
+// console.log(onlyNumbers);
+
+// .forEach()
+// const fruits = ["mango", "papaya", "pineapple", "apple"];
+
+// Iterate over fruits below
+// fruits.forEach((fruits) => {
+//   console.log(`I want to eat a ${fruits}`);
+// });
+
+// const printFruits = (elements) => {
+//   console.log(`I want to eat a ${elements}`);
+// };
+
+// fruits.forEach(printFruits);
+
+// .map()
+// const animals = [
+//   "Hen",
+//   "elephant",
+//   "llama",
+//   "leopard",
+//   "ostrich",
+//   "Whale",
+//   "octopus",
+//   "rabbit",
+//   "lion",
+//   "dog",
+// ];
+
+// // Create the secretMessage array below
+// const secretMessage = animals.map((animal) => {
+//   return animal[0];
+// });
+
+// console.log(secretMessage.join(""));
+
+// // .map()
+// const bigNumbers = [100, 200, 300, 400, 500];
+
+// // Create the smallNumbers array below
+// const smallNumbers = bigNumbers.map((number) => {
+//   return number / 100;
+// });
+
+// console.log(smallNumbers);
+
+// .filter()
+// const randomNumbers = [375, 200, 3.14, 7, 13, 852];
+
+// // Call .filter() on randomNumbers below
+// const smallNumbers = randomNumbers.filter((num) => {
+//   if (num < 250) {
+//     return num;
+//   }
+// });
+
+// console.log(smallNumbers);
+
+// const favoriteWords = [
+//   "nostalgia",
+//   "hyperbole",
+//   "fervent",
+//   "esoteric",
+//   "serene",
+// ];
+
+// // Call .filter() on favoriteWords below
+
+// const longFavoriteWords = favoriteWords.filter((word) => {
+//   if (word.length > 7) {
+//     return word;
+//   }
+// });
+
+// console.log(longFavoriteWords);
